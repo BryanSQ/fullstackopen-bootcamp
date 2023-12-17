a8dbb5802977578cd8133a4e16da4fe6072f2cc4
Bryan Josue Sandí Quirós <brysq@estudiantec.cr>
2023-12-17 13:00:26 -0600

    validations

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
-
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
@@ -63,7 +65,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -80,6 +82,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -98,7 +101,15 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    note, 
+    {
+      new: true,
+      runValidators: true,
+      context: 'query'
+    }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
